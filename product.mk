3d16502a9a1b29f7d8f2f4c02258ec120ad1db6d
LuK1337 <priv.luk@gmail.com>
2020-04-21 17:20:32 +0200

    extra: Use bash as default shell for adb
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Change-Id: I839f24c5247a1c3e3eda92ed6e9165ebdd3ff8a1
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- product.mk
+++ product.mk
@@ -31,3 +31,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     BromiteWebViewOverlay \
     LineageUpdaterOverlay
+
+# Default ADB shell prompt
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.adb.shell=/system_ext/bin/bash
