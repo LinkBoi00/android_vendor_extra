9c1581a6794f56b4861f5ad4fc350f215f4b74f9
LinkBoi00 <linkdevel@protonmail.com>
2023-02-12 12:21:10 +0200

    extra: external: ih8sn: Correct script variable check
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- external/ih8sn/fetch-ih8sn.sh
+++ external/ih8sn/fetch-ih8sn.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if [ -z ${VENDOR_PREBUILTS_PATH+x} ]; then
+if [ -z ${VENDOR_EXTERNAL_PATH+x} ]; then
     echo -e "Error: Script is not ran from build-setup.sh."
 else
     wget -q https://github.com/SebaUbuntu/ih8sn/archive/refs/heads/master.zip -O $VENDOR_EXTERNAL_PATH/ih8sn/latest-release.zip
