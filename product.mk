29f6f177c4e27c9e84670f84dce059756a0beb0c
Giovanni Ricca <giovanniricca@proton.me>
2024-03-11 01:41:02 +0200

    extra: Inherit from Android Go configuration
    Signed-off-by: Elias Dimopoulos <linkdevel@protonmail.com>

--- product.mk
+++ product.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit Android Go Makefile
+$(call inherit-product, vendor/extra/config/go.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
